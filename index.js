2126caffa81164f23ea5da33f0ed25dd75831d05
vatiza <rakibur.mac@gmail.com>
2024-05-01 16:50:31 +0600

    jwt token genareate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -19,8 +20,6 @@ const client = new MongoClient(uri, {
   },
 });
 
-
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +30,18 @@ async function run() {
       .db("cardoctorDB")
       .collection("orderCustomer");
 
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h", 
+      });
+      console.log(token)
+      res.send({token});
+    });
+
+    // servieces routes
     app.get("/services", async (req, res) => {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
